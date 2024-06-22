df52cbbb9526f2ea5920b17a1f38fcd1133bd431
sinasezza <s.e.sezza121@gmail.com>
2024-06-22 14:07:20 +0330

    user creation logic and hashing passwords added to project.

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,9 +1,23 @@
 from fastapi import APIRouter
+from passlib.context import CryptContext
+from .. import models, schemas
 
 
 router = APIRouter()
 
 
-@router.get("/auth/")
-async def get_user():
-    return {"user": "authenticated"}
+bcrypt_context = CryptContext(schemes=['bcrypt',], deprecated='auto')
+
+
+@router.post("/auth/")
+async def create_user(user: schemas.CreateUserRequest):
+    user_model = models.Users(
+        username=user.username,
+        email=user.email,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        role=user.role,
+        hashed_password=bcrypt_context.hash(user.password),
+        is_active=True
+    )
+    return user_model
--- backend/schemas.py
+++ backend/schemas.py
@@ -18,3 +18,13 @@ class TodosRequest(BaseModel):
             }
         }
     }
+
+
+
+class CreateUserRequest(BaseModel):
+    username: str = Field(min_length=3, max_length=20)
+    email: str = Field(min_length=6, max_length=100)
+    first_name: str = Field(min_length=3, max_length=20)
+    last_name: str = Field(min_length=3, max_length=20)
+    password: str = Field(min_length=6, max_length=255)
+    role: str = Field(min_length=3)
