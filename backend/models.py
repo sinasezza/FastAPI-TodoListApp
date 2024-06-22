d794afe0238d873c8e03355a0adbc6579dbd600f
sinasezza <s.e.sezza121@gmail.com>
2024-06-22 13:46:03 +0330

    users model and foreign key added to todos model.

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,26 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from .database import Base
 
 
+class Users(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+    
+
 class Todos(Base):
     __tablename__ = "todos"
+    
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     description = Column(String)
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
